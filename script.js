034d021e3fc6fbc051d0d6d9cc6b0c9e0f239238
laylaalura <layla.hermacki@escola.pr.gov.br>
2026-05-25 10:59:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,58 @@
+// CONTADOR ANIMADO
 
+let contador = document.getElementById("contador");
+
+let numero = 0;
+
+function animarContador(){
+
+  let intervalo = setInterval(() => {
+
+    numero += 50;
+
+    contador.textContent = numero;
+
+    if(numero >= 5000){
+
+      clearInterval(intervalo);
+
+    }
+
+  }, 40);
+
+}
+
+window.addEventListener("load", animarContador);
+
+
+// FORMULÁRIO
+
+const formulario = document.getElementById("formulario");
+
+formulario.addEventListener("submit", function(evento){
+
+  evento.preventDefault();
+
+  let nome = document.getElementById("nome").value;
+
+  alert("Obrigado pela participação, " + nome + "!");
+
+  formulario.reset();
+
+});
+
+
+// ACESSIBILIDADE
+
+const contrasteBtn = document.getElementById("contrasteBtn");
+
+contrasteBtn.addEventListener("click", () => {
+
+  document.body.classList.toggle("alto-contraste");
+
+});
+
+
+// DEPURAÇÃO
+
+console.log("Site carregado com sucesso!");
